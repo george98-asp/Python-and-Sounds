4f903d2721db304c543931d0b5df4f4907620183
george98-asp <76065771+george98-asp@users.noreply.github.com>
2020-12-16 11:40:59 +0200

    Create realDFT.py

---
+++ realDFT.py
@@ -0,0 +1,19 @@
+Real signal Dft
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 64
+k0 = 7
+x = np.cos(2* np.pi * k0 / N * np.arange(N))
+
+X = np.array([])
+
+for k in range(N):
+  s = np.exp(1j * 2 * np.pi * k / N * np.arange(N))
+  X = np.append(X, sum(x*np.conjugate(s)))
+  
+plt.plot (np.arange(N), abs(x))
+plt.axis([0, N-1, 0, N])
+
+plt.show()
