0ac3971fc65b6e8ab9b8f69417010d052432ba83
george98-asp <76065771+george98-asp@users.noreply.github.com>
2020-12-16 11:25:30 +0200

    Create complexsine.py

---
+++ complexsine.py
@@ -0,0 +1,18 @@
+In this file I am showing how to create a complex sinusoid using Python Packages. For execute follow the instructions in the sine.py file.
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+N = 500
+k = 3
+n = np.arange(-N/2, N/2)
+s = np.exp (1j * 2 * np.pi *k *n / N)
+
+plt.plot (n, np.real(s))
+plt.axis([-N/2, N/2-1, -1, 1])
+plt.xlabel ('n')
+plt.ylabel ('amplitude')
+
+plt.show()
